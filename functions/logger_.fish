062afb59a03f26042bdb2fb70b44a4b6413cfc12
Laurent OF Fough <laurent.fough@gmail.com>
2016-11-24 23:21:14 -0800

    logger_.fish: cleanup

--- functions/logger_.fish
+++ functions/logger_.fish
@@ -4,10 +4,13 @@ set -g __logger_format_WARN "<fg:yellow>%s</fg> <fg:purple>[%s]</fg> :: <fg:blue
 set -g __logger_format_ERROR "<fg:ffAA00>%s</fg> <fg:purple>[%s]</fg> :: <fg:yellow>[%s]</fg>"
 set -g __logger_format_FATAL "<fg:red>%s</fg> <fg:purple>[%s]</fg> :: <fg:red>[%s]</fg>"
 set -g __logger_format_CRITICAL "<bg:red><fg:white>%s</fg></bg> <fg:purple>[%s]</fg> :: <fg:red>[%s]</fg>"
+
 set -g __logger_severities_labels "DEBUG" "INFO" "WARN" "ERROR" "FATAL" "CRITICAL"
-set -g __logger_severities 1 2 3 4 5
-set -g __logger_dir_preferred "$HOME/log/fish"
+set -g __logger_severities 1 2 3 4 5 6
+
+set -g __logger_dir_preferred "~/log/fish"
 set -g __logger_dir_alternate "/tmp/fish"
+
 
 if test -d "$__logger_dir_preferred/logger_"
   set -g __logger_dir "$__logger_dir_preferred/logger_"
@@ -18,15 +21,16 @@ else
   set -g __logger_dir "$__logger_dir_alternate/logger_"
 end
 
+
 set -g __logger_file_DEBUG "$__logger_dir/logger_$__logger_severities[1]_$__logger_severities_labels[1]"
-set -g __logger_file_INFO "$__logger_dir/logger_$__logger_severities[1]_$__logger_severities_labels[1]"
-set -g __logger_file_WARN "$__logger_dir/logger_$__logger_severities[2]_$__logger_severities_labels[2]"
-set -g __logger_file_ERROR "$__logger_dir/logger_$__logger_severities[3]_$__logger_severities_labels[3]"
-set -g __logger_file_FATAL "$__logger_dir/logger_$__logger_severities[4]_$__logger_severities_labels[4]"
-set -g __logger_file_CRITICAL "$__logger_dir/logger_$__logger_severities[5]_$__logger_severities_labels[5]"
+set -g __logger_file_INFO "$__logger_dir/logger_$__logger_severities[1]_$__logger_severities_labels[2]"
+set -g __logger_file_WARN "$__logger_dir/logger_$__logger_severities[2]_$__logger_severities_labels[3]"
+set -g __logger_file_ERROR "$__logger_dir/logger_$__logger_severities[3]_$__logger_severities_labels[4]"
+set -g __logger_file_FATAL "$__logger_dir/logger_$__logger_severities[4]_$__logger_severities_labels[5]"
+set -g __logger_file_CRITICAL "$__logger_dir/logger_$__logger_severities[5]_$__logger_severities_labels[6]"
+
 
 function logger_ -a message -a severity -d "Empower your log capabilities to the maximum level"
-
   set -l label
   set -l format
 
@@ -34,17 +38,18 @@ function logger_ -a message -a severity -d "Empower your log capabilities to the
     set severity 1
   end
   set label $__logger_severities_labels[$severity]
-    # echo "label: $label"
   set format "__logger_format_"$label
-    # echo "format: $format"
-    # echo "debug_file: $__logger_dir/logger_$__logger_severities[$severity]_$__logger_severities_labels[$severity]"
 
-  # cprintf $$format $label (date) $message | gsed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' | tee -a $__logger_dir/logger_$__logger_severities[$severity]_$__logger_severities_labels[$severity]
   cprintf $$format $label (date) $message | tee -a $__logger_dir/logger_$__logger_severities[$severity]_$__logger_severities_labels[$severity]
+
+ # todo: add feature to direct logging to: silent(to-file), loud(to-stdout) or both
+ # todo: add ability to set/get environmental var that indicates if global logging is silent/loud/both
+
+ # test: strip colour/s from tee -> file
+  # cprintf $$format $label (date) $message | gsed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' | tee -a $__logger_dir/logger_$__logger_severities[$severity]_$__logger_severities_labels[$severity]
   # gstdbuf --input=0 --output=0 --error=0 cprintf $$format $label (date) $message | tee -a $__logger_file_$severity_$__logger_severities_labels[$severity]
   # gstdbuf --input=0 --output=0 --error=0 echo $$format $label (date) $message | tee -a $__logger_file_$severity_$__logger_severities_labels[$severity]
   # echo $$format $label (date) $message | gsed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' | tee -a $__logger_file_$severity_$__logger_severities_labels[$severity]
-
 end
 
 
